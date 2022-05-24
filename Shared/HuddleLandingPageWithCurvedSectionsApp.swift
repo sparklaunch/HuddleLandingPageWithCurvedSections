caf6b44cc89e27188a5bc9255730f7b16ab5b127
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-24 12:49:52 +0900

    Initial Commit

---
+++ Shared/HuddleLandingPageWithCurvedSectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleLandingPageWithCurvedSectionsApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HuddleLandingPageWithCurvedSectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
